a8e6049e68f6ef095976b87c2da4013db1b2afd6
EoghanONeill <oneill.eoghan.1@gmail.com>
2025-01-21 13:53:02 +0100

    Created MNL example

--- server.R
+++ server.R
@@ -1,15 +1,32 @@
-pts <- seq(from = -2.5, to = 2.5, length = 200)
+pts <- seq(from = -10, to = 10, length = 700)
 
 shinyServer(function(input, output, session) {
 
   output$plot1 <- renderPlot({
     
     ## Evaluate probabilities and plot
-    b0 <- input$b0
-    b1 <- input$b1
-    vals <- exp(b0 + b1 * pts) / (1 + exp(b0 + b1 * pts))
-    plot(pts, vals, xlim = c(-2.5, 2.5), ylim = c(0, 1), lwd = 2, col = "darkgoldenrod3", type = "l",
-         xlab = "X", ylab = "Prob(Y = 1|X = x)")
+    b0_j1 <- input$b0_j1
+    b1_j1 <- input$b1_j1
+    b0_j2 <- input$b0_j2
+    b1_j2 <- input$b1_j2
+    # b0_j3 <- 1
+    # b1_j3 <- 1
+    vals1 <- exp(b0_j1 + b1_j1 * pts) / (1 + exp(b0_j1 + b1_j1 * pts) + exp(b0_j2 + b1_j2 * pts))
+    vals2 <- exp(b0_j2 + b1_j2 * pts) / (1 + exp(b0_j1 + b1_j1 * pts) + exp(b0_j2 + b1_j2 * pts))
+    vals3 <- 1 / (1 + exp(b0_j1 + b1_j1 * pts) + exp(b0_j2 + b1_j2 * pts))
+    
+    plot(pts, vals1, xlim = c(-5, 5), ylim = c(0, 1), lwd = 2, col = "darkgoldenrod3", type = "l",
+         xlab = "X", ylab = "Prob(Y = j|X = x)")
+    lines(pts, vals2, #xlim = c(-5, 5), ylim = c(0, 1), lwd = 2, 
+         col = "red", type = "l"#,
+         # xlab = "X", ylab = "Prob(Y = 2|X = x)"
+         )
+    lines(pts, vals3, #xlim = c(-5, 5), ylim = c(0, 1), lwd = 2, 
+         col = "blue", type = "l"#,
+         # xlab = "X", ylab = "Prob(Y = 3|X = x)"
+         )
+    
+    
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,8 +1,10 @@
 shinyUI(pageWithSidebar(
-  headerPanel('Logistic Function'),
+  headerPanel('Multinomial Logistic Regression Probabilities'),
   sidebarPanel(
-    sliderInput('b0', 'Select intercept parameter', value = 0, min = -10, max = 10, step = 0.25),
-    sliderInput('b1', 'Select slope parameter', value = 1, min = -10, max = 10, step = 0.25)
+    sliderInput('b0_j1', 'Select intercept parameter for j=1', value = -1, min = -10, max = 10, step = 0.25),
+    sliderInput('b1_j1', 'Select slope parameter for j=1', value = 1, min = -10, max = 10, step = 0.25),
+    sliderInput('b0_j2', 'Select intercept parameter for j=2', value = 1, min = -10, max = 10, step = 0.25),
+    sliderInput('b1_j2', 'Select slope parameter for j=2', value = 0.5, min = -10, max = 10, step = 0.25)  
   ),
   mainPanel(
     plotOutput('plot1', width = "600px", height = "600px")
